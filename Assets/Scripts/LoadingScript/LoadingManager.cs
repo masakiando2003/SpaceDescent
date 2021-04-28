74f68a1d4bbc02a2118d3e4777e17efc5b1fcfbe
Lam <a18dc596@dhw.ac.jp>
2021-04-29 00:23:28 +0900

    Fix Loading Scene Index out boundry problem

--- Assets/Scripts/LoadingScript/LoadingManager.cs
+++ Assets/Scripts/LoadingScript/LoadingManager.cs
@@ -10,79 +10,102 @@ using UnityEngine.UI;
 
 public class LoadingManager : MonoBehaviour
 {
-     [SerializeField] GameObject dialogText;
-    [SerializeField] Canvas[] stageProp;
-    [SerializeField] PlayableDirector[] playableStage;
+    [SerializeField] GameObject dialogText;
+    [SerializeField] private GameObject[] stages;
     [SerializeField] GameObject blackScreenImageObject;
     [SerializeField] float fadeTime = 2f;
+    [SerializeField] float changeStageTime = 1f;
 
-    private static int playstage;
+    private static int playStage;
+
+    private IEnumerator enableStageProp;
 
     // Start is called before the first frame update
     enum PlayScene
     {
         Introduction,
-        second, third, fourth, fifth, sixth, last,
-        FadeScreen, ChangeScene
+        second,
+        third,
+        fourth,
+        fifth,
+        sixth,
+        last,
+        FadeScreen,
+        ChangeScene
     }
+
     void Start()
     {
         Initialized();
-//        playableStage[0].enabled= false;
     }
 
     private void Initialized()
     {
-        for (int i = 0; i < stageProp.Length; i++)
+        for (int i = 0; i < stages.Length; i++)
         {
-            stageProp[i].enabled= false;
+            stages[i].SetActive(false);
         }
+
+        StartCoroutine(UpdateStageChange());
         blackScreenImageObject.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-
-        playstage = dialogText.GetComponent<TypeWriterEffect>().PlayScene;
-        if (playstage != (int)PlayScene.ChangeScene) {
-            StopCoroutine((EnablePlaySceneAnim(playstage)));
-            StartCoroutine(EnablePlaySceneAnim(playstage));
-            Debug.Log(playstage);
+        if (playStage < (int) PlayScene.FadeScreen)
+        {
+            if (enableStageProp != null) StopCoroutine((EnablePlaySceneAnim(playStage)));
+            enableStageProp = EnablePlaySceneAnim(playStage);
+            StartCoroutine(enableStageProp);
+            //     Debug.Log(playstage);
         }
-        else if (playstage == (int)PlayScene.FadeScreen)
+        else if (playStage == (int) PlayScene.FadeScreen)
         {
             StartCoroutine(FadeIntoStage());
         }
-        else if(playstage ==(int) PlayScene.ChangeScene)
+        else if (playStage == (int) PlayScene.ChangeScene)
         {
+            StopAllCoroutines();
             SceneManager.LoadScene("Stage");
         }
+    }
+
+    void EnableStageProp(PlayScene stage)
+    {
+        int StageNum = (int) stage;
+        stages[StageNum].SetActive(true);
+        for (int i = 0; i < stages.Length; i++)
+        {
+            if (i != StageNum)
+                stages[i].SetActive(false);
+        }
+    }
+
+    private IEnumerator UpdateStageChange()
+    {
+        while (playStage < (int) PlayScene.FadeScreen)
+        {
+            playStage = dialogText.GetComponent<TypeWriterEffect>().PlayScene;
+            Debug.Log("Playstage is " + playStage);
+
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(1f);
+        Debug.Log("Finish Update playstage");
     }
 
     private IEnumerator FadeIntoStage()
     {
         blackScreenImageObject.SetActive(true);
         yield return new WaitForSeconds(fadeTime);
-        playstage = (int)PlayScene.ChangeScene;
+        playStage = (int) PlayScene.ChangeScene;
     }
 
-    IEnumerator EnablePlaySceneAnim(int playstage)
+    private IEnumerator EnablePlaySceneAnim(int playstage)
     {
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(changeStageTime);
         EnableStageProp((PlayScene) playstage);
-    }
-    void EnableStageProp(PlayScene stage)
-    {
-        int StageNum = (int) stage;
-        if (stageProp[StageNum] == null) return;
-        stageProp[StageNum].enabled = false;
-         stageProp[StageNum].enabled = true;
-
-        for (int i = 0; i < stageProp.Length; i++)
-        {
-            if (i != StageNum)
-                stageProp[i].enabled = false;
-        }
+        yield return null;
     }
 }
